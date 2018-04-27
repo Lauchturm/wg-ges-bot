21ec29eadacae898ab00a8f47843298e5a50f162
Julian Schmitz <julian.schmitz@flixbus.com>
2018-04-27 17:31:22 +0200

    added explanations to params_template

--- params_template.py
+++ params_template.py
@@ -1,3 +1,3 @@
-admin_chat_id = "42" # Get your user id, e.g. ask @FalconGate_Bot /get_my_id
-tor_pwd = None # None is default
-token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" # Get your token by chatting with Telegram's @BotFather
+admin_chat_id = 123456789  # Get your user id, e.g. ask @FalconGate_Bot /get_my_id
+tor_pwd = None  # None is default. If you set one up at your tor config you need to provide it here
+token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"  # Get your token by chatting with Telegram's @BotFather
